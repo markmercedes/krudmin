960eff14935aada8e0712d83c87f654437ef5d8c
Marcos Mercedes <marcos.mercedesn@gmail.com>
2017-12-22 17:38:07 -0400

    Refactor NavigationItems

--- lib/krudmin/navigation_items.rb
+++ lib/krudmin/navigation_items.rb
@@ -24,37 +24,40 @@ module Krudmin
 
     def items
       @items ||= [
-        build_node(:dashboard, routes.krudmin_path, icon: :tachometer),
-        node_for("Cars", 'car', :car),
-      ]
-    end
-
-    def node_for(label, resource, icon)
-      build_node(label, "#", items: links_for(resource), icon: icon)
-    end
-
-    def links_for(resource)
-      links = [
-        build_node(I18n.t("krudmin.actions.manage"), routes.send("admin_#{resource.pluralize}_path"), icon: :list),
-        build_node(I18n.t("krudmin.actions.add_new"), routes.send("new_admin_#{resource}_path"), icon: :plus),
-      ]
-    end
-
-    def build_node(*args)
-      Node.new(*args)
+        Krudmin::NavigationItems::Node.new(:dashboard, routes.krudmin_path, icon: :tachometer)
+      ].concat(Krudmin::Config.menu_items.call)
     end
 
     class Node
       include Enumerable
 
-      attr_reader :label, :link, :visible, :items, :icon
+      attr_reader :label, :link, :visible, :items, :icon, :module_path
 
-      def initialize(label, link, visible: true, items: [], icon: :file)
-        @label, @link, @visible, @items, @icon = label, link, visible, items, icon
+      def initialize(label, link, visible: true, items: [], icon: :file, module_path: nil)
+        @label, @link, @visible, @items, @icon, @module_path = label, link, visible, items, icon, module_path
       end
 
       def each(&block)
         items.select(&:visible).each(&block)
+      end
+
+      class << self
+        def routes
+          Rails.application.routes.url_helpers
+        end
+
+        def node_for(label, resource, visible: true, icon: :file, module_path: nil, manage: true, add: true)
+          new(label, "#", items: links_for(resource, module_path, manage: manage, add: add), icon: icon, visible: visible)
+        end
+
+        def links_for(resource, module_path = '', manage: true, add: true)
+          module_path = "#{module_path}_" if module_path.present?
+
+          links = [
+            new(I18n.t("krudmin.actions.manage"), routes.send("#{module_path}#{resource.pluralize}_path"), icon: :list, visible: manage),
+            new(I18n.t("krudmin.actions.add_new"), routes.send("new_#{module_path}#{resource}_path"), icon: :plus, visible: add),
+          ]
+        end
       end
 
       private
