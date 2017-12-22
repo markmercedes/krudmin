e6b3caa81bcd4ea1073f6daebf88f4c99be17a5c
Marcos Mercedes <marcos.mercedesn@gmail.com>
2017-12-22 17:38:15 -0400

    Initialize menu from the outside

---
+++ spec/test_app/config/initializers/krudmin.rb
@@ -0,0 +1,9 @@
+Krudmin::Config.with do |config|
+  config.current_user_method(&:current_user)
+
+  config.menu_items = -> { [
+      Krudmin::NavigationItems::Node.node_for("Admin Cars", 'car', module_path: :admin, manage: false, icon: :car),
+      Krudmin::NavigationItems::Node.node_for("Console Cars", 'car', module_path: :console, add: false, icon: :car)
+    ]
+  }
+end
