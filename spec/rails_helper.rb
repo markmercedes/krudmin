612e8c927f407bbcee4be0745a904638bb63ff9a
Marcos Mercedes <marcos.mercedesn@gmail.com>
2017-12-22 12:46:38 -0400

    Remove shoulda matchers initializer in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,11 +15,4 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
-
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
-  end
 end
