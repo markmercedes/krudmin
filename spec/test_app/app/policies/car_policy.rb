8d5bdda23307b8a6492a12493cb0c610c7d66191
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-01-03 16:33:47 -0400

    Add noon policy for cars controller in test app

---
+++ spec/test_app/app/policies/car_policy.rb
@@ -0,0 +1,42 @@
+class CarPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      scope
+    end
+  end
+
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
+end
