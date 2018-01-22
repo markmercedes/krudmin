9452906353b049586447fa7fb8e1b05192d70b0c
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-01-22 11:32:08 -0400

    Extract form_id from form object

--- lib/krudmin/action_buttons/save_button.rb
+++ lib/krudmin/action_buttons/save_button.rb
@@ -2,11 +2,17 @@ module Krudmin
   module ActionButtons
     class SaveButton < Base
       def form_id
-        @form_id ||= html_options.fetch(:form_id)
+        @form_id ||= extract_form_id
       end
 
       def render_form
         render_partial(:form, form_id: form_id)
+      end
+
+      private
+
+      def extract_form_id
+        html_options[:form_id] ? html_options[:form_id] : html_options.delete(:form).options.fetch(:html).fetch(:id)
       end
     end
   end
