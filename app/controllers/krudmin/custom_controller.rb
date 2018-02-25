418345e760539d935bf8581fc88b2771723cf258
marcosmercedesn <marcos.mercedesn@gmail.com>
2018-02-25 16:12:02 -0400

    Create custom controller non dependent on resource manager

---
+++ app/controllers/krudmin/custom_controller.rb
@@ -0,0 +1,16 @@
+module Krudmin
+  class CustomController < Krudmin::Config.parent_controller.constantize
+    include Krudmin::KrudminControllerSupport
+    include Krudmin::ActionButtonsSupport
+
+    layout Krudmin::Config.theme
+
+    before_action :set_view_path
+
+    private
+
+    def set_view_path
+      lookup_context.prefixes.append Krudmin::Config.theme
+    end
+  end
+end
---
+++ spec/controllers/admin/customs_controller_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe Admin::CustomsController, type: :controller do
+  describe "GET index" do
+    it "renders the index" do
+      get :index
+
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET show" do
+    it "renders a show page for the given resource" do
+      get :show, params: {id: 1}
+
+      expect(response).to be_success
+    end
+  end
+end
