b7ad6bd67030e5c2102cd8875406fe9e0a7b8248
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-03-08 12:49:58 -0400

    Add comma after last array item

--- app/helpers/krudmin/application_helper.rb
+++ app/helpers/krudmin/application_helper.rb
@@ -3,7 +3,7 @@ module Krudmin
     def body_classes
       [
         cookies["brand-minimized"] == "true" ? "brand-minimized" : nil,
-        cookies["sidebar-minimized"] == "true" ? "sidebar-minimized" : nil
+        cookies["sidebar-minimized"] == "true" ? "sidebar-minimized" : nil,
       ].compact.join(" ")
     end
   end
