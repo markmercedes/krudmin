614a119b2a060623a7a7eef874129b4f43dc4d29
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-03-26 01:20:33 -0400

    Use fallback route on view for New and Cancel action buttons

--- lib/krudmin/action_buttons/cancel_button.rb
+++ lib/krudmin/action_buttons/cancel_button.rb
@@ -4,7 +4,6 @@ module Krudmin
       def initialize(page, view_context, action_path = nil, html_options = {}, &block)
         super
 
-        @action_path ||= view_context.resource_root
         @page = :base
       end
 
--- lib/krudmin/action_buttons/new_button.rb
+++ lib/krudmin/action_buttons/new_button.rb
@@ -3,8 +3,6 @@ module Krudmin
     class NewButton < Base
       def initialize(page, view_context, action_path = nil, html_options = {}, &block)
         super
-
-        @action_path ||= view_context.new_resource_path
       end
     end
   end
