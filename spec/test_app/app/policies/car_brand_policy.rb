8283dcb90ea0d32a725d2d6e1f79c29f8667c2d0
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-03-26 14:38:45 -0400

    Dumb policies rules

--- spec/test_app/app/policies/car_brand_policy.rb
+++ spec/test_app/app/policies/car_brand_policy.rb
@@ -1,7 +1,42 @@
-class CarBrandPolicy < CarPolicy
+class CarBrandPolicy
   class Scope < Struct.new(:user, :scope)
     def resolve
       scope
     end
+  end
+
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
   end
 end
