4b4d4d730e1b0e451b4e8780fdc9a824729d69ee
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-04-21 01:23:09 -0400

    Add root route for admin namespace

--- spec/test_app/config/routes.rb
+++ spec/test_app/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "admin/cars#index"
 
   namespace :admin do
+    root to: "cars#index"
+
     resources :car_brands
 
     resources :customs, only: [:index, :show]
