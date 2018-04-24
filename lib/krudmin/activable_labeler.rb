be57bab1f36caaf26219877c8c6ea4dfbc688d06
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-04-24 18:50:24 -0400

    Remove arbre gem

--- lib/krudmin/activable_labeler.rb
+++ lib/krudmin/activable_labeler.rb
@@ -1,11 +1,14 @@
 module Krudmin
   module ActivableLabeler
+    include ActionView::Helpers::TagHelper
+    include ActionView::Context
+
     def label_for_active(value)
-      Arbre::Context.new { span(class: "badge badge-success") { value } }
+      content_tag(:span, class: "badge badge-success") { value }
     end
 
     def label_for_inactive(value)
-      Arbre::Context.new { span(class: "badge badge-danger") { value } }
+      content_tag(:span, class: "badge badge-danger") { value }
     end
   end
 end
--- spec/lib/krudmin/fields/boolean_spec.rb
+++ spec/lib/krudmin/fields/boolean_spec.rb
@@ -2,7 +2,6 @@ require "spec_helper"
 require "#{Dir.pwd}/lib/krudmin/activable_labeler"
 require "#{Dir.pwd}/lib/krudmin/fields/base"
 require "#{Dir.pwd}/lib/krudmin/fields/boolean"
-require "arbre"
 
 describe Krudmin::Fields::Boolean do
   subject { described_class.new(:level, model) }
@@ -11,7 +10,7 @@ describe Krudmin::Fields::Boolean do
     let(:model) { double(level: true) }
 
     it "generates a decorated label with positive value inside" do
-      expect(subject.to_s.to_s).to eq("<span class=\"badge badge-success\">Yes</span>\n")
+      expect(subject.to_s.to_s).to eq("<span class=\"badge badge-success\">Yes</span>")
     end
   end
 
@@ -19,7 +18,7 @@ describe Krudmin::Fields::Boolean do
     let(:model) { double(level: false) }
 
     it "generates a decorated label negative true value inside" do
-      expect(subject.to_s.to_s).to eq("<span class=\"badge badge-danger\">No</span>\n")
+      expect(subject.to_s.to_s).to eq("<span class=\"badge badge-danger\">No</span>")
     end
   end
 end
