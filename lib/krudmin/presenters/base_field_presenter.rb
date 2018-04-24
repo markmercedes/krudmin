a1ba5fb6ec21511981836551333284c1d268c12b
Oliver <oliver312@gmail.com>
2018-04-24 16:40:59 -0400

    Allows setting the partial form and infers the partial directory name from the field instead of the presenter name.

--- lib/krudmin/presenters/base_field_presenter.rb
+++ lib/krudmin/presenters/base_field_presenter.rb
@@ -76,11 +76,11 @@ module Krudmin
       end
 
       def partial_scope
-        @partial_scope ||= self.class.name.demodulize.split("Field").first.underscore
+        @partial_scope ||= field.class.name.demodulize.underscore
       end
 
       def default_locals
-        { field: field, form: form, input_options: input_options, attribute: attribute }
+        { field: field, form: form, input_options: input_options, attribute: attribute, options: options }
       end
 
       def form
