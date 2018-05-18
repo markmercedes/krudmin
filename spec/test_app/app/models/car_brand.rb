7bf0bf247a6dad8212a0bd5d8863620215311f82
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-05-17 22:10:55 -0400

    Add search_by_term scope to CarBrand

--- spec/test_app/app/models/car_brand.rb
+++ spec/test_app/app/models/car_brand.rb
@@ -2,4 +2,6 @@ class CarBrand < ApplicationRecord
   validates :description, uniqueness: true, presence: true
 
   has_many :cars
+
+  scope :search_by_term, -> (term) { where("description ILIKE ?", "%#{term}%").limit(10) }
 end
