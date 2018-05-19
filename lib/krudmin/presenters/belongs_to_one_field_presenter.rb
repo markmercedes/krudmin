b0eaa118eadec88a8c5d6bad7f17b031c7a6da5a
Marcos Mercedes <marcos.mercedesn@gmail.com>
2018-05-19 17:25:11 -0400

    Make better use of inheritance

--- lib/krudmin/presenters/belongs_to_one_field_presenter.rb
+++ lib/krudmin/presenters/belongs_to_one_field_presenter.rb
@@ -1,19 +1,6 @@
 module Krudmin
   module Presenters
-    class BelongsToOneFieldPresenter < BaseFieldPresenter
-      def render_form
-        render_partial(partial_form, options: options, form_fields: form_fields)
-      end
-
-      def fields_partial
-        field.options.fetch(:fields_partial, :form_fields)
-      end
-
-      private
-
-      def form_fields
-        render_partial_to_string(fields_partial, options: options)
-      end
+    class BelongsToOneFieldPresenter < HasOneFieldPresenter
     end
   end
 end
