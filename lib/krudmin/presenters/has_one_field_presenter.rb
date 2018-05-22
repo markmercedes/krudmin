827acf55acc59989689115a46f49f4776edcb371
Alvin Dickson <dickson.alvin@gmail.com>
2018-05-22 11:40:24 -0400

    displaying form on load for belongs_to_one

--- lib/krudmin/presenters/has_one_field_presenter.rb
+++ lib/krudmin/presenters/has_one_field_presenter.rb
@@ -2,11 +2,19 @@ module Krudmin
   module Presenters
     class HasOneFieldPresenter < BaseFieldPresenter
       def render_form
-        render_partial(partial_form, options: options, form_fields: form_fields)
+        render_partial(partial_form, options: options, form_fields: form_fields, display_fields_on_load: display_fields_on_load?)
       end
 
       def fields_partial
         field.options.fetch(:fields_partial, :form_fields)
+      end
+
+      def required?
+        field.options.fetch(:required, false)
+      end
+
+      def display_fields_on_load?
+        required? || field.model.send(attribute)&.id
       end
 
       private
