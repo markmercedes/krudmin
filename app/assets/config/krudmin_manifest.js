ae4fd93f2c9919b549ee8fcd782a3c82a72a8957
Marcos Mercedes <marcos.mercedesn@gmail.com>
2020-01-07 02:38:41 -0400

    Include core theme in manifest

--- app/assets/config/krudmin_manifest.js
+++ app/assets/config/krudmin_manifest.js
@@ -1,2 +1,4 @@
 //= link_directory ../javascripts/krudmin .js
 //= link_directory ../stylesheets/krudmin .css
+//= link krudmin/core_theme/application.js
+//= link krudmin/core_theme/application.css
